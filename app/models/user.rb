7bf7edaebae778f78085e109c5beda04a35543f3
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-06-20 20:32:06 +0300

    feat: add validations at model and database levels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, presence: true
+  validates :email, uniqueness: true # model level not db level
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
 end
