0cd172fbf3e94532feb952d7fcc6bd0b19ec2fc1
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-06-20 20:12:17 +0300

    feat: implement user CRUD actions and remove address column from users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,41 @@
 class UsersController < ApplicationController
   def index
-    
+    @users= User.all
   end
 
   def new
-    
+    @user= User.new
   end
 
   def create
-    
+    @user= User.new(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number])
+    if @user.save
+      redirect_to @user, notice:"User Created Succesfully"
+    else
+      render :new, status: :unprocessable_content
+    end
   end
 
   def show
-    
+    @user= User.find(params[:id])
   end
 
   def edit
-    
+    @user= User.find(params[:id])
   end
 
   def update
-    
+    @user= User.find(params[:id])
+    if @user.update(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number])
+      redirect_to @user, notice: "User Updated Successfully"
+    else
+      render :edit, status: :unprocessable_content
+    end
   end
 
   def destroy
-    
+    @user= User.find(params[:id])
+    @user.destroy
+    redirect_to :posts
   end
 end
