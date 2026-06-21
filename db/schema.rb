a4feab6f227949a9e4e064c192689387fa8915f7
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-06-21 23:03:46 +0300

    feat: add editors resource with CRUD functionality and associated views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_21_194638) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_21_200254) do
+  create_table "editors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
